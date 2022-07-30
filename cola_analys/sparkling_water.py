c61a3a388e0e9ad195aa4225e7aa63e734d8d710
Господин Ведущий <sustaff@mail.ru>
2022-07-30 14:07:52 +0300

    Класс "газированная вода"(В-во,Жидкость,Вода,Газ)

--- cola_analys/sparkling_water.py
+++ cola_analys/sparkling_water.py
@@ -1,11 +1,32 @@
-from gas import Gas
-from water import Water
+class Substance:
+    ruClassName = "Вещество"
+
+
+class Liquid(Substance):
+    ruClassName = "Жидкость"
+
+class Water(Liquid):
+    ruClassName = "Вода"
+    def info_water(self):
+        return(f"\nЯ - {Substance.ruClassName}\nМоё агрегатное состояние - {Liquid.ruClassName}\nЯ - {Water.ruClassName}")
+
+class Gas(Substance):
+    ruClassName = "Газ"
 
 
 class Sparkling_water(Water, Gas):
     ruClassName = "Газированная вода"
     def info_sparkling_water(self):
         return(f'Я-{Sparkling_water.ruClassName}\nПолучаюсь при смешивании {Gas.ruClassName} + {Water.ruClassName}')
+
+water=Sparkling_water()
+
+print(water.info_water())
+print(water.info_sparkling_water())
+
+
+
+
 
 
 
