7cf799d45768409b1b4dcc41e6c51f55681f5505
Господин Ведущий <sustaff@mail.ru>
2022-07-30 13:27:28 +0300

    Жидкость

--- cola_analys/liquid.py
+++ cola_analys/liquid.py
@@ -3,16 +3,3 @@ from substance import Substance
 
 class Liquid(Substance):
     ruClassName = "Жидкость"
-
-    def info(self):
-        """Аргумент density.в чем измеряется плотность жидкости"""
-
-        print(f"Я - {Substance.ruClassName}")
-        print(f"Я - {Liquid.ruClassName}")
-        print(f"Моя плотность измеряется в {self}")
-
-
-Liquid.info('кг/м³')
-
-
-
